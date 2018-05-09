9b21801bfb109271912bfadbaa26629e33961808
Martin Hermant <martin.hermant@gmail.com>
2018-05-09 14:08:54 +0200

    remove old files

--- src/components/ForumPostList.vue
+++ src/components/ForumPostList.vue
@@ -21,13 +21,13 @@
 import forumPost from './ForumPost.vue'
 import forumPostEditor from './ForumPostEditor.vue'
 import forumAPI from '../api/forum'
-import ServerMessage from './ServerMessage.vue'
+// import ServerMessage from './ServerMessage.vue'
 
 
 
 // import Vue from 'vue'
 export default {
-  components: { forumpost: forumPost, posteditor: forumPostEditor, ServerMessage },
+  components: { forumpost: forumPost, posteditor: forumPostEditor },
   data () {
     return {
       postIds: [],
